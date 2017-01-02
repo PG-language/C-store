99833e1730cf0b706e93d86ab14adeca73f9136f
aqeel saadi <aqeel saadi@RAK-AS>
2017-01-02 11:39:58 +0300

    Add project files.

---
+++ first program/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace first_program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello world");
+            Console.ReadKey();
+            Console.Write("ayman"); Console.ReadKey();
+
+
+            int my_valum = 5;
+            Console.Write("{}", (my_valum)); 
+            Console.ReadKey(); 
+
+        }
+    }
+}
